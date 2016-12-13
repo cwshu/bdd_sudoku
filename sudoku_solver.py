dd93579cbad317c365357553580dcdd2d416648e
susu <u1240976@gmail.com>
2016-12-14 05:04:29 +0800

    fix bit issue by implementing binary_encode_number_range(), but current implementation is too slow to solve 9x9 puzzle.
    change variable name: bits => binary_size

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -58,7 +58,9 @@ class BoolOp():
         # BoolOp.depth -= 1
         return pyeda_expr
 
-    ### debug use ###
+    def __bool__(self):
+        return self.operands_count() != 0
+
     def operands_count(self):
         return len(self.operands)
 
@@ -98,9 +100,54 @@ def bit_value(value, bit):
     value = value >> bit
     return value % 2
 
+def binary_encode_number_range(stop_num, binary_size, var_name):
+    '''binary encoding an integer variable, whose range is between 0 and stop-1.
+    '''
+
+    assert stop_num <= 2**binary_size, 'number range should be in the binary size'
+    if stop_num <= 0:
+        return []
+
+    # algorithm note
+    # 
+    # generate 0 ~ 24
+    # stop_num = 25 = 11001 = 10000 + 1000 + 1
+    #
+    # [00 ~ 15] 10000 => 10000 => 0xxxx => ^x4
+    # [16 ~ 23]  1000 => 11000 => 10xxx =>  x4 & ^x3
+    # [     24]     1 => 11001 => 11000 =>  x4 &  x3 & ^x2 & ^x1 & ^x0
+    #
+    #                   base_val
+    #
+    # encoding = And(0xxxx, 10xxx, 11000) = ...
+
+    base_val = 0
+    result = BoolOp('OR')
+    for bit in range(binary_size-1, -1, -1): # (binary = 4) => (3, 2, 1, 0)
+        if bit_value(stop_num, bit) == 0:
+            continue
+
+        # bit value == 1
+        base_val += 1 << bit 
+
+        a = BoolOp('AND')
+        for bit2 in range(binary_size-1, bit, -1):
+            prefix = '' if bit_value(base_val, bit2) == 1 else '~'
+            a.append_operand('{prefix}{name}{bit}'.format(prefix=prefix, name=var_name, bit=bit2))
+
+        a.append_operand('~{name}{bit}'.format(name=var_name, bit=bit))
+        result.append_operand(a)
+
+    return result
+
 def encode_puzzle(puzzle, size_square):
 
-    bits = 2
+    binary_size = int(math.log2(size_square - 1)) + 1
+    # 4 => 0 ~ 3 =>   00 ~   11 => binary size 2
+    # 5 => 0 ~ 4 =>  000 ~  100 => binary size 3
+    # 8 => 0 ~ 7 =>  000 ~  111 => binary size 3
+    # 9 => 0 ~ 8 => 0000 ~ 1000 => binary size 4
+
     combination_2_list = combination_2(size_square)
     # variable: x_<row>_<col>_<bit>
 
@@ -130,7 +177,7 @@ def encode_puzzle(puzzle, size_square):
             # print(len(restrictions.operands))
 
             inequality = BoolOp('OR') # one bit difference is enough
-            for bit in range(bits):
+            for bit in range(binary_size):
                 # x[row][col1][bit] != x[row][col2][bit]
                 # use xor operator for != (in boolean expression)
                 bit_diff = 'x_{row}_{col1}_{bit} ^ x_{row}_{col2}_{bit}'.format(row=row, col1=col1, col2=col2, bit=bit)
@@ -147,7 +194,7 @@ def encode_puzzle(puzzle, size_square):
         for row1, row2 in combination_2_list:
 
             inequality = BoolOp('OR') # one bit difference is enough
-            for bit in range(bits):
+            for bit in range(binary_size):
                 # x[row1][col][bit] != x[row2][col][bit]
                 # use xor operator for != (in boolean expression)
                 bit_diff = 'x_{row1}_{col}_{bit} ^ x_{row2}_{col}_{bit}'.format(row1=row1, row2=row2, col=col, bit=bit)
@@ -164,7 +211,7 @@ def encode_puzzle(puzzle, size_square):
         for index1, index2 in combination_2_list:
 
             inequality = BoolOp('OR') # one bit difference is enough
-            for bit in range(bits):
+            for bit in range(binary_size):
                 # x[row1][col1][bit] != x[row2][col2][bit]
                 # use xor operator for != (in boolean expression)
                 row1, col1 = block_to_row_col(block, index1, size_square)
@@ -178,16 +225,27 @@ def encode_puzzle(puzzle, size_square):
 
         restrictions.append_operand(block_restrict)
 
+    print(size_square, binary_size)
+    print(binary_encode_number_range(size_square, binary_size, 'x') == True)
+
     # 4. fill in number
     for row in range(size_square):
         for col in range(size_square):
             if puzzle[row][col] == 0:
+                # from 0 ~ size_square - 1
+                range_restrict = binary_encode_number_range(size_square, binary_size, 'x_{}_{}_'.format(row, col))
+
+                # if stop_num == power(2, binary_size), then range_restrict == empty boolean expression
+                # which means BoolOp with no operand, then __bool__() is false
+                if range_restrict:
+                    restrictions.append_operand(range_restrict)
+
                 continue
 
             # for each cell != 0
             # set every bits to stable value
             cell_value = BoolOp('AND')
-            for bit in range(bits):
+            for bit in range(binary_size):
                 if bit_value(puzzle[row][col], bit) == 1:
                     cell_value.append_operand( 'x_{}_{}_{}'.format(row, col, bit) )
                 else:
